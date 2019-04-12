dfb74110ca1d524bb7032cf75884c0b57cdb8133
ron reyes <ronelle.e.reyes@accenture.com>
2019-04-12 16:39:25 +0800

    feedbackv2: partial codes

---
+++ myTNB.iOS/Home/Feedback/Feedback Entry/FeedbackEntryViewController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UIKit;
+
+namespace myTNB
+{
+    public partial class FeedbackEntryViewController : UIViewController
+    {
+        public FeedbackEntryViewController(IntPtr handle) : base(handle)
+        {
+        }
+    }
+}
