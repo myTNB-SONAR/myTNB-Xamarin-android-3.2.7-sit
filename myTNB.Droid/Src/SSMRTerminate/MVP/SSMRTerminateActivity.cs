059e1b2eec1213f4e33c78f270a6aa337b071d19
Lin Siong Soh <linsiong.soh@accenture.com>
2019-07-25 13:26:45 +0800

    Create SSMRTerminate Activity

---
+++ myTNB.Droid/Src/SSMRTerminate/MVP/SSMRTerminateActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace myTNB_Android.Src.SSMRTerminate.MVP
+{
+    [Activity(Label = "SSMRTerminateActivity")]
+    public class SSMRTerminateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
