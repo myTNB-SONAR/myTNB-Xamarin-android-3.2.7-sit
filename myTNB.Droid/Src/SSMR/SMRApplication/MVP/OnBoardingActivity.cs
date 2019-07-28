5f50afe7e89ffd326681aefd18736a7024307531
Chris <christoper.sosa@accenture.com>
2019-07-28 08:46:36 +0800

    Added walkthrough

---
+++ myTNB.Droid/Src/SSMR/SMRApplication/MVP/OnBoardingActivity.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using myTNB_Android.Src.Base.Activity;
+
+namespace myTNB_Android.Src.SSMR.SMRApplication.MVP
+{
+    [Activity(Label = "OnBoardingActivity", Theme = "@style/Theme.Dashboard")]
+    public class OnBoardingActivity : BaseAppCompatActivity
+    {
+        public override int ResourceId()
+        {
+            return Resource.Layout.OnboardingSSMRView;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
