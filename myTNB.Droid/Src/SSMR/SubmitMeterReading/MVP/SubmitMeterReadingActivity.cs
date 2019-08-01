44afd4ea2967ae0555af09ec5f9a59df8d27f4eb
Chris <christoper.sosa@accenture.com>
2019-08-02 02:28:49 +0800

    Updated with meter reading type

--- myTNB.Droid/Src/SSMR/SubmitMeterReading/MVP/SubmitMeterReadingActivity.cs
+++ myTNB.Droid/Src/SSMR/SubmitMeterReading/MVP/SubmitMeterReadingActivity.cs
@@ -14,9 +14,15 @@ using myTNB_Android.Src.Base.Activity;
 
 namespace myTNB_Android.Src.SSMR.SubmitMeterReading.MVP
 {
+    public enum METER_READING_TYPE
+    {
+        KWH,KVARH,KW
+    }
+
     [Activity(Label = "Submit Meter Reading", Theme = "@style/Theme.Dashboard", MainLauncher = true)]
     public class SubmitMeterReadingActivity : BaseToolbarAppCompatActivity
     {
+        private METER_READING_TYPE meterType;
         public override int ResourceId()
         {
             return Resource.Layout.SubmitMeterReadingLayout;
@@ -30,11 +36,60 @@ namespace myTNB_Android.Src.SSMR.SubmitMeterReading.MVP
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            meterType = METER_READING_TYPE.KWH;
             // Create your application here
-            LinearLayout linearLayout = FindViewById(Resource.Id.kwCard) as LinearLayout;
-            EditText et = linearLayout.FindViewById(Resource.Id.new_reading_1) as EditText;
-            et.Text = "9";
+            PopulateMeterReadingCard(METER_READING_TYPE.KWH);
+            PopulateMeterReadingCard(METER_READING_TYPE.KVARH);
+            PopulateMeterReadingCard(METER_READING_TYPE.KW);
+        }
+
+        public void PopulateMeterReadingCard(METER_READING_TYPE type)
+        {
+            LinearLayout linearLayoutContainer;
+            string typeId;
+
+            switch (type)
+            {
+                case METER_READING_TYPE.KWH:
+                    linearLayoutContainer = FindViewById(Resource.Id.kwhCard) as LinearLayout;
+                    typeId = "kWh";
+                    break;
+                case METER_READING_TYPE.KVARH:
+                    linearLayoutContainer = FindViewById(Resource.Id.kVARhCard) as LinearLayout;
+                    typeId = "kVARh";
+                    break;
+                default:
+                    linearLayoutContainer = FindViewById(Resource.Id.kwCard) as LinearLayout;
+                    typeId = "kW";
+                    break;
+            }
+
+            TextView firstPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_1) as TextView;
+            TextView secondPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_2) as TextView;
+            TextView thirdPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_3) as TextView;
+            TextView fourthPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_4) as TextView;
+            TextView fifthPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_5) as TextView;
+            TextView sixthPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_6) as TextView;
+            TextView seventhPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_7) as TextView;
+            TextView eigthPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_8) as TextView;
+            TextView ninethPrevDigit = linearLayoutContainer.FindViewById(Resource.Id.previous_reading_9) as TextView;
+
+            EditText firstDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_1) as EditText;
+            EditText secondDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_2) as EditText;
+            EditText thirdDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_3) as EditText;
+            EditText fourthDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_4) as EditText;
+            EditText fifthDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_5) as EditText;
+            EditText sixthDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_6) as EditText;
+            EditText seventhDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_7) as EditText;
+            EditText eigthDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_8) as EditText;
+            EditText ninethDigit = linearLayoutContainer.FindViewById(Resource.Id.new_reading_9) as EditText;
+
+            TextView readingErrorMessage = linearLayoutContainer.FindViewById(Resource.Id.reading_error_validation_msg) as TextView;
+            TextView readingMeterType = linearLayoutContainer.FindViewById(Resource.Id.reading_meter_type) as TextView;
+
+            readingErrorMessage.Text = "";
+            readingMeterType.Text = typeId;
+
         }
     }
 }
