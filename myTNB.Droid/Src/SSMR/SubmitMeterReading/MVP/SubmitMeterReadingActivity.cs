8bc11a9918ca9f3fedd49fa5d8029be5b8024fe7
Chris <christoper.sosa@accenture.com>
2019-08-19 04:56:38 +0800

    Removed mock data

--- myTNB.Droid/Src/SSMR/SubmitMeterReading/MVP/SubmitMeterReadingActivity.cs
+++ myTNB.Droid/Src/SSMR/SubmitMeterReading/MVP/SubmitMeterReadingActivity.cs
@@ -331,28 +331,28 @@ namespace myTNB_Android.Src.SSMR.SubmitMeterReading.MVP
             isFirstLaunch = true;
 
             validationStateList = new List<MeterValidation>();
-            //Mock Data - Start
-            SMRValidateRegisterDetailList = DummyData();
-            SetUpMeterCards(SMRValidateRegisterDetailList);
-            selectedAccount = new AccountData();
-            selectedAccount.AccountNum = "1010101010";
-            //Mock Data - End
-            //Bundle intentExtras = Intent.Extras;
+            ////Mock Data - Start
+            //SMRValidateRegisterDetailList = DummyData();
+            //SetUpMeterCards(SMRValidateRegisterDetailList);
+            //selectedAccount = new AccountData();
+            //selectedAccount.AccountNum = "1010101010";
+            ////Mock Data - End
+            Bundle intentExtras = Intent.Extras;
 
-            //if (intentExtras.ContainsKey(Constants.SELECTED_ACCOUNT))
-            //{
-            //    selectedAccount = JsonConvert.DeserializeObject<AccountData>(intentExtras.GetString(Constants.SELECTED_ACCOUNT));
-            //}
+            if (intentExtras.ContainsKey(Constants.SELECTED_ACCOUNT))
+            {
+                selectedAccount = JsonConvert.DeserializeObject<AccountData>(intentExtras.GetString(Constants.SELECTED_ACCOUNT));
+            }
 
-            //if (intentExtras.ContainsKey(Constants.SMR_RESPONSE_KEY))
-            //{
-            //    ssmrActivityInfoResponse = JsonConvert.DeserializeObject<SMRActivityInfoResponse>(intentExtras.GetString(Constants.SMR_RESPONSE_KEY));
-            //    if (ssmrActivityInfoResponse.Response != null && ssmrActivityInfoResponse.Response.Data != null)
-            //    {
-            //        SMRValidateRegisterDetailList = ssmrActivityInfoResponse.Response.Data.SMRMROValidateRegisterDetails;
-            //        SetUpMeterCards(SMRValidateRegisterDetailList);
-            //    }
-            //}
+            if (intentExtras.ContainsKey(Constants.SMR_RESPONSE_KEY))
+            {
+                ssmrActivityInfoResponse = JsonConvert.DeserializeObject<SMRActivityInfoResponse>(intentExtras.GetString(Constants.SMR_RESPONSE_KEY));
+                if (ssmrActivityInfoResponse.Response != null && ssmrActivityInfoResponse.Response.Data != null)
+                {
+                    SMRValidateRegisterDetailList = ssmrActivityInfoResponse.Response.Data.SMRMROValidateRegisterDetails;
+                    SetUpMeterCards(SMRValidateRegisterDetailList);
+                }
+            }
             OnGenerateTooltipData();
         }
 
