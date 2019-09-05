ddc7276915d8c2d64c7d62aaff9c7a1fdcb7bd29
ron reyes <ronelle.e.reyes@accenture.com>
2019-09-05 17:22:04 +0800

    Logic update for inline validation and popup. Added i18n constants.

--- myTNB.iOS/Payment/PaymentConstants.cs
+++ myTNB.iOS/Payment/PaymentConstants.cs
@@ -2,6 +2,10 @@
 {
     public static class PaymentConstants
     {
-        public static string I18N_EnterAmount = "Enter amount";
+        public static string Pagename_SelectBills = "SelectBills";
+
+        public static string I18N_EnterAmount = "Enter Amount";
+        public static string I18N_MinimumMandatoryPayment = "minimumMandatoryPayment";
+        public static string I18N_MinimumPayAmount = "minimumPayAmount";
     }
 }
