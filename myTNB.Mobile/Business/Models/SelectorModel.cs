e1b65e8b3814d99aca6d8f8db966255be59d2cf2
Chris <christoper.sosa@accenture.com>
2019-11-04 10:31:13 +0800

    Updated myTNB.Mobile module

---
+++ myTNB.Mobile/Business/Models/SelectorModel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace myTNB
+{
+    public class SelectorModel
+    {
+        [JsonProperty("id")]
+        public string ID { set; get; }
+        [JsonProperty("key")]
+        public string Key { set; get; }
+        [JsonProperty("value")]
+        public string Value { set; get; }
+        [JsonProperty("description")]
+        public string Description { set; get; }
+    }
+
+    public class PopupSelectorModel
+    {
+        [JsonProperty("Title")]
+        public string Title { set; get; }
+        [JsonProperty("Description")]
+        public string Description { set; get; }
+        [JsonProperty("CTA")]
+        public string CTA { set; get; }
+        [JsonProperty("Type")]
+        public string Type { set; get; }
+    }
+}
