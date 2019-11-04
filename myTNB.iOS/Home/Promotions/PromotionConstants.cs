39fe710beacc1cb23eb2d758f6941928d780f078
ron reyes <ronelle.e.reyes@accenture.com>
2019-11-04 09:52:06 +0800

    no message

--- myTNB.iOS/Home/Promotions/PromotionConstants.cs
+++ myTNB.iOS/Home/Promotions/PromotionConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class PromotionConstants
     {
-        public static string Pagename_Promotion = "Promotion";
+        public static string Pagename_Promotion = "Promotions";
 
         public static string I18N_Title = "title";
         public static string I18N_NoPromotions = "noPromotions";
