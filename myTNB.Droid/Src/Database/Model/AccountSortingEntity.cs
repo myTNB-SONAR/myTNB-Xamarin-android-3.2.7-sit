9d6703f08a333450c05da6ef1ca0e184b4111763
Lin Siong Soh <linsiong.soh@accenture.com>
2019-11-19 13:58:02 +0800

    initiate rearrange account

---
+++ myTNB.Droid/Src/Database/Model/AccountSortingEntity.cs
@@ -0,0 +1,114 @@
+﻿using myTNB_Android.Src.AddAccount.Models;
+using myTNB_Android.Src.SummaryDashBoard.Models;
+using myTNB_Android.Src.Utils;
+using Newtonsoft.Json;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myTNB_Android.Src.Database.Model
+{
+    [Table("AccountSortingEntity")]
+    public class AccountSortingEntity
+    {
+        [PrimaryKey, Column("EmailAddress")]
+        public string EmailAddress { get; set; }
+
+        [Column("AccountList")]
+        public string AccountList { get; set; }
+
+        public static int CreateTable()
+        {
+            var db = DBHelper.GetSQLiteConnection();
+            return (int)db.CreateTable<AccountSortingEntity>();
+        }
+
+        public static void CreateTableAsync(SQLiteAsyncConnection db)
+        {
+            db.CreateTableAsync<AccountSortingEntity>();
+        }
+
+        public static int InsertOrReplace(string emailAddress
+            , string accountList)
+        {
+            var db = DBHelper.GetSQLiteConnection();
+            var newRecord = new AccountSortingEntity()
+            {
+                EmailAddress = emailAddress,
+                AccountList = accountList
+            };
+
+            int newRecordRow = db.InsertOrReplace(newRecord);
+
+            return newRecordRow;
+        }
+
+        public static int InsertOrReplace(string emailAddress
+                , List<CustomerBillingAccount> customerBillingAccounts)
+        {
+            int newRecordRow = 0;
+            try
+            {
+                var db = DBHelper.GetSQLiteConnection();
+
+                string accountList = JsonConvert.SerializeObject(customerBillingAccounts);
+
+                var newRecord = new AccountSortingEntity()
+                {
+                    EmailAddress = emailAddress,
+                    AccountList = accountList
+                };
+
+                newRecordRow = db.InsertOrReplace(newRecord);
+            }
+            catch (Exception e)
+            {
+                Utility.LoggingNonFatalError(e);
+            }
+
+            return newRecordRow;
+        }
+
+        public static List<CustomerBillingAccount> List(string email)
+        {
+            List<CustomerBillingAccount> customerAccounts = new List<CustomerBillingAccount>();
+
+            if (HasItems(email))
+            {
+                try
+                {
+
+                }
+                catch (Exception e)
+                {
+                    Utility.LoggingNonFatalError(e);
+                }
+            }
+
+            return customerAccounts;
+        }
+
+        public static bool HasItems(string email)
+        {
+            var db = DBHelper.GetSQLiteConnection();
+            return db.Query<AccountSortingEntity>("SELECT * FROM AccountSortingEntity WHERE EmailAddress = ?", email).Count > 0;
+        }
+
+        public static List<CustomerBillingAccount> ReturnList(string email)
+        {
+            var db = DBHelper.GetSQLiteConnection();
+
+            List<CustomerBillingAccount> customerAccounts = new List<CustomerBillingAccount>();
+
+            AccountSortingEntity item = db.Query<AccountSortingEntity>("SELECT * FROM AccountSortingEntity WHERE EmailAddress = ?", email).ToList()[0]
+
+            if (item != null)
+            {
+                
+            }
+
+            return customerAccounts;
+        }
+    }
+}
