992fbb811875ac44b9c3de8a47f9b32664ac29c8
Lin Siong Soh <linsiong.soh@accenture.com>
2019-11-22 14:00:35 +0800

    First Select for Account list

--- myTNB.Droid/Src/Login/MVP/LoginPresenter.cs
+++ myTNB.Droid/Src/Login/MVP/LoginPresenter.cs
@@ -697,10 +697,10 @@ namespace myTNB_Android.Src.Login.MVP
                 {
                     foreach (Account acc in list)
                     {
-                        bool isSelected = ctr == 0 ? true : false;
-                        int rowChange = CustomerBillingAccount.InsertOrReplace(acc, isSelected);
-                        ctr++;
+                        int rowChange = CustomerBillingAccount.InsertOrReplace(acc, false);
                     }
+                    CustomerBillingAccount.RemoveSelected();
+                    CustomerBillingAccount.MakeFirstAsSelected();
                 }
             }
             catch (Exception e)
