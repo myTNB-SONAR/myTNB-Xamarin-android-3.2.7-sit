e6c4ecdafa2c14e085c085710c68b53cf260d4ad
Roberto Jr Cestina <roberto.f.cestina.jr@accenture.com>
2019-11-22 16:12:10 +0800

    code clean up

---
+++ myTNB.iOS/Home/More/My Account/ProfileConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace myTNB
+{
+    public static class ProfileConstants
+    {
+        //PageName
+        public static string PageName = "Profile";
+
+        //I18N Keys
+        public static string I18N_NavTitle = "navTitle";
+    }
+}
