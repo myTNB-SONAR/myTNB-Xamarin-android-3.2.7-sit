5259181c0e860f157fd0a1f228093d7ad0720c8f
ron reyes <ronelle.e.reyes@accenture.com>
2020-01-06 17:23:35 +0800

    What's new Updates.

--- myTNB.iOS/Home/Promotions/PromotionsViewController.cs
+++ myTNB.iOS/Home/Promotions/PromotionsViewController.cs
@@ -1,7 +1,6 @@
 using Foundation;
 using System;
 using UIKit;
-using myTNB.Dashboard.DashboardComponents;
 using CoreGraphics;
 using myTNB.Home.Promotions;
 using myTNB.SitecoreCMS.Model;
@@ -9,6 +8,7 @@ using myTNB.SQLite.SQLiteDataManager;
 using System.Collections.Generic;
 using System.Diagnostics;
 using myTNB.SitecoreCMS;
+using CoreAnimation;
 
 namespace myTNB
 {
@@ -17,11 +17,14 @@ namespace myTNB
         public PromotionsViewController(IntPtr handle) : base(handle) { }
 
         public bool IsDelegateNeeded;
-        private UIImageView _imgViewNoPromotions;
-        private UILabel _lblDetails;
-        private TitleBarComponent _titleBarComponent;
-        private UIView _headerView;
+
+        private UIImageView _imgViewNoPromotions, _bgImageView;
         private bool isPromoDetailScreen;
+        private nfloat _navBarHeight;
+        private UIView _navbarView;
+        private UILabel _lblNavTitle, _lblDetails, _lblRefreshMsg;
+        private CAGradientLayer _gradientLayer;
+        private CustomUIButtonV2 _btnRefresh;
 
         public override void ViewDidLoad()
         {
@@ -29,7 +32,7 @@ namespace myTNB
             NavigationController.NavigationBarHidden = true;
             base.ViewDidLoad();
             Debug.WriteLine("PROMOTION DID LOAD");
-            NotifCenterUtility.AddObserver((Foundation.NSString)"LanguageDidChange", LanguageDidChange);
+            NotifCenterUtility.AddObserver((NSString)"LanguageDidChange", LanguageDidChange);
             SetNavigationBar();
             promotionsTableView.Frame = new CGRect(0, DeviceHelper.IsIphoneXUpResolution() ? 88 : 64
                 , View.Frame.Width, View.Frame.Height - 49 - (DeviceHelper.IsIphoneXUpResolution() ? 88 : 64));
@@ -39,19 +42,24 @@ namespace myTNB
         {
             Debug.WriteLine("DEBUG >>> PROMOTIONS LanguageDidChange");
             base.LanguageDidChange(notification);
-            _titleBarComponent?.SetTitle(GetI18NValue(PromotionConstants.I18N_Title));
+            if (_lblNavTitle != null)
+            {
+                _lblNavTitle.Text = GetI18NValue(PromotionConstants.I18N_Title);
+            }
         }
 
         public override void ViewWillAppear(bool animated)
         {
             base.ViewWillAppear(animated);
-            //NavigationController.SetNavigationBarHidden(true, true);
             Debug.WriteLine("PROMOTION WILL APPEAR");
+            RemoveRefresh();
+            RemoveNoPromotionView();
             if (!isPromoDetailScreen)
             {
                 promotionsTableView.Hidden = true;
                 promotionsTableView.Source = new PromotionsDataSource(this, new List<PromotionsModel>());
                 promotionsTableView.ReloadData();
+
                 if (DataManager.DataManager.SharedInstance.IsPromotionFirstLoad)
                 {
                     NetworkUtility.CheckConnectivity().ContinueWith(networkTask =>
@@ -72,7 +80,7 @@ namespace myTNB
                             }
                             else
                             {
-                                DisplayNoDataAlert();
+                                DisplayRefresh();
                             }
                         });
                     });
@@ -94,37 +102,99 @@ namespace myTNB
             base.ViewWillDisappear(animated);
         }
 
-        /// <summary>
-        /// Sets the value for null end date.
-        /// </summary>
-        /// <returns>The value for null end date.</returns>
-        /// <param name="promotions">Promotions.</param>
-        private List<PromotionsModel> SetValueForNullEndDate(List<PromotionsModel> promotions)
-        {
-            List<PromotionsModel> promotionList = new List<PromotionsModel>();
-            foreach (PromotionsModel promo in promotions)
-            {
-                if (string.IsNullOrEmpty(promo.PromoEndDate))
-                {
-                    DateTime nowDate = DateTime.Today.Date;
-                    DateTime endDate = nowDate.AddDays(90);
-                    promo.PromoEndDate = endDate.ToString("yyyyMMdd");
-                }
-                promotionList.Add(promo);
-            }
-            return promotionList;
-        }
-
         private void SetNavigationBar()
         {
-            GradientViewComponent gradientViewComponent = new GradientViewComponent(View, true, 64, true);
-            _headerView = gradientViewComponent.GetUI();
-            _titleBarComponent = new TitleBarComponent(_headerView);
-            UIView titleBarView = _titleBarComponent.GetUI();
-            _titleBarComponent.SetTitle(GetI18NValue(PromotionConstants.I18N_Title));
-            _titleBarComponent.SetPrimaryVisibility(true);
-            _headerView.AddSubview(titleBarView);
-            View.AddSubview(_headerView);
+            if (NavigationController != null && NavigationController.NavigationBar != null)
+            {
+                NavigationController.NavigationBar.Hidden = true;
+                _navBarHeight = NavigationController.NavigationBar.Frame.Height;
+            }
+
+            _navbarView = new UIView(new CGRect(0, 0, ViewWidth, DeviceHelper.GetStatusBarHeight() + _navBarHeight))
+            {
+                BackgroundColor = UIColor.Clear
+            };
+
+            UIView viewTitleBar = new UIView(new CGRect(0, DeviceHelper.GetStatusBarHeight() + GetScaledHeight(4)
+                , _navbarView.Frame.Width, GetScaledHeight(24)));
+            _lblNavTitle = new UILabel(new CGRect(0, 0, ViewWidth, GetScaledHeight(24)))
+            {
+                Font = TNBFont.MuseoSans_16_500,
+                Text = GetI18NValue(PromotionConstants.I18N_Title),
+                TextColor = UIColor.White,
+                TextAlignment = UITextAlignment.Center
+            };
+
+            viewTitleBar.AddSubview(_lblNavTitle);
+            _navbarView.AddSubview(viewTitleBar);
+
+            UIColor startColor = MyTNBColor.GradientPurpleDarkElement;
+            UIColor endColor = MyTNBColor.GradientPurpleLightElement;
+            _gradientLayer = new CAGradientLayer
+            {
+                Colors = new[] { startColor.CGColor, endColor.CGColor }
+            };
+            _gradientLayer.StartPoint = new CGPoint(x: 0.0, y: 0.5);
+            _gradientLayer.EndPoint = new CGPoint(x: 1.0, y: 0.5);
+
+            _gradientLayer.Frame = _navbarView.Bounds;
+            _gradientLayer.Opacity = 1;
+            _navbarView.Layer.InsertSublayer(_gradientLayer, 0);
+            View.AddSubview(_navbarView);
+        }
+
+        private void DisplayRefresh()
+        {
+            _gradientLayer.Opacity = 0;
+            _bgImageView = new UIImageView(new CGRect(0, 0, ViewWidth, ViewWidth * 0.76875F))
+            {
+                Image = UIImage.FromBundle(Constants.IMG_BannerRefresh)
+            };
+
+            _lblRefreshMsg = new UILabel(new CGRect(GetScaledWidth(32)
+                      , GetYLocationFromFrame(_bgImageView.Frame, 16), ViewWidth - GetScaledWidth(64), 1000))
+            {
+                Font = TNBFont.MuseoSans_16_300,
+                TextAlignment = UITextAlignment.Center,
+                Lines = 0,
+                LineBreakMode = UILineBreakMode.WordWrap,
+                TextColor = MyTNBColor.Grey,
+                Text = GetCommonI18NValue(Constants.Common_RefreshMessage)
+            };
+            nfloat lblHeight = _lblRefreshMsg.GetLabelHeight(1000);
+            _lblRefreshMsg.Frame = new CGRect(_lblRefreshMsg.Frame.Location, new CGSize(_lblRefreshMsg.Frame.Width, lblHeight));
+            View.AddSubviews(new UIView[] { _bgImageView, _lblRefreshMsg });
+            View.SendSubviewToBack(_bgImageView);
+
+            _btnRefresh = new CustomUIButtonV2
+            {
+                Frame = new CGRect(GetScaledWidth(16), GetYLocationFromFrame(_lblRefreshMsg.Frame, GetScaledHeight(16))
+                , BaseMarginedWidth, ScaleUtility.GetScaledHeight(48)),
+                BackgroundColor = MyTNBColor.FreshGreen
+            };
+            _btnRefresh.SetTitle(GetCommonI18NValue(Constants.Common_RefreshNow), UIControlState.Normal);
+            _btnRefresh.AddGestureRecognizer(new UITapGestureRecognizer(() =>
+            {
+                ViewWillAppear(true);
+            }));
+            View.AddSubview(_btnRefresh);
+        }
+
+        private void RemoveRefresh()
+        {
+            _gradientLayer.Opacity = 1;
+            if (_bgImageView != null)
+            {
+                _bgImageView.RemoveFromSuperview();
+            }
+            if (_lblRefreshMsg != null)
+            {
+                _lblRefreshMsg.RemoveFromSuperview();
+            }
+            if (_btnRefresh != null)
+            {
+                _btnRefresh.RemoveFromSuperview();
+            }
         }
 
         private void SetSubViews()
@@ -159,7 +229,7 @@ namespace myTNB
         private void SetNoPromotionScreen()
         {
             _imgViewNoPromotions = new UIImageView(new CGRect((ViewWidth - GetScaledWidth(100)) / 2
-                , GetScaledHeight(88) + _headerView.Frame.GetMaxY()
+                , GetScaledHeight(88) + _navbarView.Frame.GetMaxY()
                 , GetScaledWidth(100), GetScaledHeight(96)))
             {
                 Image = UIImage.FromBundle("IC-Empty-Promotion")
@@ -180,6 +250,18 @@ namespace myTNB
             _lblDetails.Frame = new CGRect(_lblDetails.Frame.Location, new CGSize(_lblDetails.Frame.Width, newTitleHeight));
 
             View.AddSubviews(new UIView[] { _imgViewNoPromotions, _lblDetails });
+        }
+
+        private void RemoveNoPromotionView()
+        {
+            if (_imgViewNoPromotions != null)
+            {
+                _imgViewNoPromotions.RemoveFromSuperview();
+            }
+            if (_lblDetails != null)
+            {
+                _lblDetails.RemoveFromSuperview();
+            }
         }
 
         internal void OnPromotionItemSelect(PromotionsModel promotion)
