a7f7c96678841783920ee24bba7088c6489cd4bb
Lin Siong Soh <linsiong.soh@accenture.com>
2020-01-07 11:01:34 +0800

    Fix on Android 5.0 compatibility issue

--- myTNB.Droid/Src/myTNBMenu/Fragments/HomeMenu/MVP/HomeMenuFragment.cs
+++ myTNB.Droid/Src/myTNBMenu/Fragments/HomeMenu/MVP/HomeMenuFragment.cs
@@ -279,6 +279,17 @@ namespace myTNB_Android.Src.myTNBMenu.Fragments.HomeMenu.MVP
             presenter = new HomeMenuPresenter(this, PreferenceManager.GetDefaultSharedPreferences(this.Activity));
         }
 
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+
+
+        public override int ResourceId()
+        {
+            return Resource.Layout.HomeMenuFragmentView;
+        }
+
         public override void OnAttach(Context context)
         {
             base.OnAttach(context);
@@ -426,11 +437,14 @@ namespace myTNB_Android.Src.myTNBMenu.Fragments.HomeMenu.MVP
                     }
                 };
 
-                closeImageView.Clickable = true;
+                if (closeImageView != null)
+                {
+                    closeImageView.Clickable = true;
 
-                closeImageView.Click += delegate {
-                    OnSearchOutFocus(true);
-                };
+                    closeImageView.Click += delegate {
+                        OnSearchOutFocus(true);
+                    };
+                }
 
                 bool isGetEnergyTipsDisabled = false;
                 if (MyTNBAccountManagement.GetInstance().IsEnergyTipsDisabled())
@@ -852,7 +866,10 @@ namespace myTNB_Android.Src.myTNBMenu.Fragments.HomeMenu.MVP
                 searchEditText.OnActionViewExpanded();
                 searchEditText.RequestFocus();
                 searchEditText.SetBackgroundResource(Resource.Drawable.search_edit_bg);
-                closeImageView.Visibility = ViewStates.Visible;
+                if (closeImageView != null)
+                {
+                    closeImageView.Visibility = ViewStates.Visible;
+                }
             }
             else
             {
@@ -876,19 +893,66 @@ namespace myTNB_Android.Src.myTNBMenu.Fragments.HomeMenu.MVP
             searchEditText.SetQueryHint(GetLabelByLanguage("searchPlaceholder"));
             searchEditText.SetOnQueryTextListener(new AccountsSearchOnQueryTextListener(this,accountsAdapter));
             searchEditText.SetOnQueryTextFocusChangeListener(this);
-            EditText searchText = searchEditText.FindViewById<EditText>(searchEditText.Context.Resources.GetIdentifier("android:id/search_src_text", null, null));
-            searchText.SetTextColor(Resources.GetColor(Resource.Color.white));
-            searchText.SetHintTextColor(Resources.GetColor(Resource.Color.sixty_opacity_white));
-            searchText.SetTextSize(ComplexUnitType.Dip, 12f);
-            TextViewUtils.SetMuseoSans500Typeface(searchText);
-            searchText.SetPadding((int) DPUtils.ConvertDPToPx(34f), 0, 0, 0);
 
-            int closeViewId = searchEditText.Context.Resources.GetIdentifier("android:id/search_close_btn", null, null);
+            try
+            {
+                EditText searchText = searchEditText.FindViewById<EditText>(searchEditText.Context.Resources.GetIdentifier("android:id/search_src_text", null, null));
+                searchText.SetTextColor(Resources.GetColor(Resource.Color.white));
+                searchText.SetHintTextColor(Resources.GetColor(Resource.Color.sixty_opacity_white));
+                searchText.SetTextSize(ComplexUnitType.Dip, 12f);
+                TextViewUtils.SetMuseoSans500Typeface(searchText);
+                searchText.SetPadding((int)DPUtils.ConvertDPToPx(34f), 0, 0, 0);
+            }
+            catch (System.Exception cex)
+            {
+                Utility.LoggingNonFatalError(cex);
+            }
 
-            closeImageView = searchEditText.FindViewById<ImageView>(closeViewId);
-            closeImageView.Visibility = ViewStates.Visible;
+            try
+            {
+                int searchMagId = searchEditText.Context.Resources.GetIdentifier("android:id/search_mag_icon", null, null);
+                ImageView mSearchHintIcon = searchEditText.FindViewById<ImageView>(searchMagId);
+                mSearchHintIcon.Visibility = ViewStates.Gone;
+                mSearchHintIcon.SetImageDrawable(null);
+            }
+            catch (System.Exception cex)
+            {
+                Utility.LoggingNonFatalError(cex);
+            }
 
-            closeImageView.SetPadding(0, 0, 0, 0);
+            try
+            {
+                int searchMagId = searchEditText.Context.Resources.GetIdentifier("android:id/search_mag_icon", null, null);
+                ImageView mSearchHintIcon = searchEditText.FindViewById<ImageView>(searchMagId);
+                mSearchHintIcon.SetImageResource(0);
+            }
+            catch (System.Exception cex)
+            {
+                Utility.LoggingNonFatalError(cex);
+            }
+
+            try
+            {
+                searchEditText.SetBackgroundResource(Resource.Drawable.search_edit_bg);
+            }
+            catch (System.Exception cex)
+            {
+                Utility.LoggingNonFatalError(cex);
+            }
+
+            try
+            {
+                int closeViewId = searchEditText.Context.Resources.GetIdentifier("android:id/search_close_btn", null, null);
+                closeImageView = searchEditText.FindViewById<ImageView>(closeViewId);
+                closeImageView.Visibility = ViewStates.Visible;
+                closeImageView.SetImageResource(Resource.Drawable.search_close_bg);
+                closeImageView.SetPadding(0, 0, 0, 0);
+            }
+            catch (System.Exception cex)
+            {
+                Utility.LoggingNonFatalError(cex);
+            }
+
             searchActionContainer.Click += (s, e) =>
             {
                 if (isSearchClose)
@@ -930,11 +994,13 @@ namespace myTNB_Android.Src.myTNBMenu.Fragments.HomeMenu.MVP
                 var actionBar = act.SupportActionBar;
                 actionBar.Hide();
                 ShowBackButton(false);
+
                 if (this.presenter != null)
                 {
                     this.presenter.GetUserNotifications();
                     this.presenter.OnCheckMyServiceState();
                 }
+
                 if (newLabel != null)
                 {
                     SetNotificationIndicator();
@@ -953,17 +1019,6 @@ namespace myTNB_Android.Src.myTNBMenu.Fragments.HomeMenu.MVP
             {
                 Utility.LoggingNonFatalError(e);
             }
-        }
-
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            return base.OnCreateView(inflater, container, savedInstanceState);
-        }
-
-
-        public override int ResourceId()
-        {
-            return Resource.Layout.HomeMenuFragmentView;
         }
 
         public void OnUpdateAccountListChanged(bool isSearchSubmit)
