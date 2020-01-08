88bcc354fdd46b44327fa573d97c6a1c307aff0a
Chris <christoper.sosa@accenture.com>
2020-01-08 10:21:41 +0800

    Update RemoveAccountRequest.cs

--- myTNB.Droid/Src/MyTNBService/Request/RemoveAccountRequest.cs
+++ myTNB.Droid/Src/MyTNBService/Request/RemoveAccountRequest.cs
@@ -4,9 +4,11 @@ namespace myTNB_Android.Src.MyTNBService.Request
     public class RemoveAccountRequest : BaseRequest
     {
         public string accNum;
+        public DeviceInfoRequest deviceInf;
 
         public RemoveAccountRequest(string accNum)
         {
+            deviceInf = new DeviceInfoRequest();
             this.accNum = accNum;
         }
     }
