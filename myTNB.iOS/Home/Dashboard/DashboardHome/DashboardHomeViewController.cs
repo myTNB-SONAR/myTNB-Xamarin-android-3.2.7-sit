18b4aa681756d77eda9b82ad9d3ec81a54f4d0d8
ron reyes <ronelle.e.reyes@accenture.com>
2020-01-10 11:10:18 +0800

    Fix message not translated in Home page.

--- myTNB.iOS/Home/Dashboard/DashboardHome/DashboardHomeViewController.cs
+++ myTNB.iOS/Home/Dashboard/DashboardHome/DashboardHomeViewController.cs
@@ -378,6 +378,11 @@ namespace myTNB
                     lblGreeting.Text = greeting;
                 }
             }
+            if (_refreshScreenComponent != null)
+            {
+                ShowRefreshScreen(true);
+            }
+
             if (_accountListViewController != null)
             {
                 DataManager.DataManager.SharedInstance.AccountListIsLoaded = false;
@@ -1007,18 +1012,23 @@ namespace myTNB
             }
         }
 
+        public void ClearRefreshScreen()
+        {
+            if (_refreshScreenComponent != null)
+            {
+                if (_refreshScreenComponent.GetView() != null)
+                {
+                    _refreshScreenComponent.GetView().RemoveFromSuperview();
+                    _refreshScreenComponent = null;
+                }
+            }
+        }
+
         public void ShowRefreshScreen(bool isFail, RefreshScreenInfoModel model = null)
         {
             InvokeOnMainThread(() =>
             {
-                if (_refreshScreenComponent != null)
-                {
-                    if (_refreshScreenComponent.GetView() != null)
-                    {
-                        _refreshScreenComponent.GetView().RemoveFromSuperview();
-                        _refreshScreenComponent = null;
-                    }
-                }
+                ClearRefreshScreen();
                 _isRefreshScreenEnabled = isFail;
                 if (_isRefreshScreenEnabled)
                 {
