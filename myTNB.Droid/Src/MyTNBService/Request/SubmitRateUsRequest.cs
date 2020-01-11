68cfa83c7311601ff8bddcc8bcf36ca8b408c7fa
Chris <christoper.sosa@accenture.com>
2020-01-11 11:04:39 +0800

    Code fix for defect 3976

--- myTNB.Droid/Src/MyTNBService/Request/SubmitRateUsRequest.cs
+++ myTNB.Droid/Src/MyTNBService/Request/SubmitRateUsRequest.cs
@@ -5,27 +5,37 @@ namespace myTNB_Android.Src.MyTNBService.Request
 {
     public class SubmitRateUsRequest : BaseRequest
     {
-        public string ReferenceId, Email, DeviceId;
-        public List<InputAnswerDetails> inputAnswerDetails;
+        public InputAnswerData InputAnswer;
 
         public SubmitRateUsRequest(string referenceId, string email, string deviceId)
         {
-            ReferenceId = referenceId;
-            Email = email;
-            DeviceId = deviceId;
-            inputAnswerDetails = new List<InputAnswerDetails>();
+            InputAnswer = new InputAnswerData(referenceId, email, deviceId);
         }
 
         public void AddAnswerDetails(string wLTYQuestionId, string ratingInput, string multilineInput)
         {
-            inputAnswerDetails.Add(new InputAnswerDetails(wLTYQuestionId, ratingInput, multilineInput));
+            InputAnswer.InputAnswerDetails.Add(new InputAnswerDetailList(wLTYQuestionId, ratingInput, multilineInput));
         }
 
-        public class InputAnswerDetails
+        public class InputAnswerData
+        {
+            public string ReferenceId, Email, DeviceId;
+            public List<InputAnswerDetailList> InputAnswerDetails;
+
+            public InputAnswerData(string referenceId, string email, string deviceId)
+            {
+                ReferenceId = referenceId;
+                Email = email;
+                DeviceId = deviceId;
+                InputAnswerDetails = new List<InputAnswerDetailList>();
+            }
+        }
+
+        public class InputAnswerDetailList
         {
             public string WLTYQuestionId, RatingInput, MultilineInput;
 
-            public InputAnswerDetails(string wLTYQuestionId, string ratingInput, string multilineInput)
+            public InputAnswerDetailList(string wLTYQuestionId, string ratingInput, string multilineInput)
             {
                 WLTYQuestionId = wLTYQuestionId;
                 RatingInput = ratingInput;
