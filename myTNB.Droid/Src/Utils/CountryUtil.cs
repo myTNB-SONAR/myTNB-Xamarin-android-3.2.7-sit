3c947ed34ffbd8a516387bb8ffd97a2836b41573
Chris <christoper.sosa@accenture.com>
2020-01-21 10:23:35 +0800

    Added CountryUtil

---
+++ myTNB.Droid/Src/Utils/CountryUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace myTNB_Android.Src.Utils
+{
+    public class CountryUtil
+    {
+        private static readonly Lazy<CountryUtil>
+            lazy = new Lazy<CountryUtil>(() => new CountryUtil());
+        public static CountryUtil Instance { get { return lazy.Value; } }
+        private CountryUtil()
+        {
+        }
+
+
+    }
+}
