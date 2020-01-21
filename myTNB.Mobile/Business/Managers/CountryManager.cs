9f1d0225a73afb16131ad42d1398ee6189900bde
Chris <christoper.sosa@accenture.com>
2020-01-21 10:30:40 +0800

    Updated myTNB.Mobile module

---
+++ myTNB.Mobile/Business/Managers/CountryManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace myTNB
+{
+    public sealed class CountryManager
+    {
+        private static readonly Lazy<CountryManager> lazy = new Lazy<CountryManager>(() => new CountryManager());
+
+        public static CountryManager Instance { get { return lazy.Value; } }
+
+        private CountryManager() { }
+
+        private string CountryJSON = string.Empty;
+        private const string COUNTRY_RESOURCE_PATH = "myTNB.Mobile.Resources.Country.CountryList.json";
+
+        public void SetCountries()
+        {
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Stream stream = assembly.GetManifestResourceStream(COUNTRY_RESOURCE_PATH);
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CountryJSON = reader.ReadToEnd();
+                    Debug.WriteLine("DEBUG >> CountryJSON: " + CountryJSON);
+                }
+                Debug.WriteLine("DEBUG >> SUCCESS");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("DEBUG >> SetLanguageData: " + e.Message);
+            }
+        }
+
+        public void SetCountries(string content)
+        {
+            if (!string.IsNullOrEmpty(content) && !string.IsNullOrWhiteSpace(content))
+            {
+                CountryJSON = content;
+            }
+        }
+
+        public string CountryString
+        {
+            get
+            {
+                return CountryJSON;
+            }
+        }
+
+        public List<CountryModel> GetCountryList()
+        {
+            Countries Countries = (string.IsNullOrEmpty(CountryJSON) || string.IsNullOrWhiteSpace(CountryJSON)) ? new Countries()
+                : JsonConvert.DeserializeObject<Countries>(CountryJSON);
+            if (Countries != null && Countries.CountryList != null)
+            {
+                return Countries.CountryList;
+            }
+
+            return new List<CountryModel>();
+        }
+
+        public CountryModel GetCountryInfo(string countryCode)
+        {
+            List<CountryModel> countryList = GetCountryList();
+            if (countryList != null && countryList.Count > 0)
+            {
+                int index = countryList.FindIndex(x => x.CountryCode.ToUpper() == countryCode.ToUpper());
+                if (index > -1)
+                {
+                    return countryList[index];
+                }
+            }
+            return new CountryModel();
+        }
+
+        public Dictionary<string, List<CountryModel>> GetCountryDictionary()
+        {
+            Dictionary<string, List<CountryModel>> countryDictionary = new Dictionary<string, List<CountryModel>>(); ;
+            List<CountryModel> countryList = GetCountryList();
+            if (countryList != null && countryList.Count > 0)
+            {
+                for (int i = 0; i < countryList.Count; i++)
+                {
+                    CountryModel countryInfo = countryList[i];
+                    string countryName = countryInfo.CountryName;
+                    string firstChar = countryName[0].ToString().ToUpper();
+                    if (countryDictionary.ContainsKey(firstChar))
+                    {
+                        List<CountryModel> list = countryDictionary[firstChar];
+                        list.Add(countryInfo);
+                    }
+                    else
+                    {
+                        countryDictionary.Add(firstChar, new List<CountryModel> { countryInfo });
+                    }
+                }
+            }
+            return countryDictionary;
+        }
+    }
+}
---
+++ myTNB.Mobile/Business/Models/CountryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace myTNB
+{
+    public class Countries
+    {
+        public List<CountryModel> CountryList { set; get; } = new List<CountryModel>();
+    }
+
+    public class CountryModel
+    {
+        public string CountryCode { set; get; } = string.Empty;
+        public string CountryName { set; get; } = string.Empty;
+        public string CountryISDCode { set; get; } = string.Empty;
+    }
+}
