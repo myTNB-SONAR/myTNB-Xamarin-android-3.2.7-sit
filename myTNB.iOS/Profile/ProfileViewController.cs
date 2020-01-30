0217ddb6b4da62a35f2dc96c56effdfbab7d1e6b
ron reyes <ronelle.e.reyes@accenture.com>
2020-01-30 11:09:43 +0800

    Update animation timings.

--- myTNB.iOS/Profile/ProfileViewController.cs
+++ myTNB.iOS/Profile/ProfileViewController.cs
@@ -726,7 +726,7 @@ namespace myTNB
         {
             _viewNotificationMsg.Hidden = false;
             _viewNotificationMsg.Alpha = 1.0f;
-            UIView.Animate(2, 1, UIViewAnimationOptions.CurveEaseOut, () =>
+            UIView.Animate(1, 3, UIViewAnimationOptions.CurveEaseOut, () =>
             {
                 _viewNotificationMsg.Alpha = 0.0f;
             }, () =>
