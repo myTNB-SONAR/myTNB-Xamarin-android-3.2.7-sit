d1cd75c9de9814ee7c717d36dfb68ec95069151e
80000529 <syahmi.shafawiics@tnb.com.my>
2020-07-14 15:39:35 +0800

    Submit Feedback Feature

---
+++ myTNB.iOS/Model/Feedback/FeedbackUpdateDetailsModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace myTNB.Model
+{
+    public class FeedbackUpdateDetailsModel
+    {
+        public int FeedbackUpdInfoType { set; get; }
+        public string FeedbackUpdInfoTypeDesc { set; get; }
+        public string FeedbackUpdInfoValue { set; get; }
+    }
+}
