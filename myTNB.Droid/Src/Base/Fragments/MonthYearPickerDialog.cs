fd5454c05a4517f68cc96f80da08d5f42e89f8dd
Lin Siong Soh <linsiong.soh@accenture.com>
2020-08-04 11:39:41 +0800

    Month picker update

--- myTNB.Droid/Src/Base/Fragments/MonthYearPickerDialog.cs
+++ myTNB.Droid/Src/Base/Fragments/MonthYearPickerDialog.cs
@@ -1,11 +1,12 @@
 ﻿
 using System;
-
+using System.Collections.Generic;
 using Android.App;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
 using myTNB_Android.Src.Base.Activity;
+using myTNB_Android.Src.Base.Models;
 using myTNB_Android.Src.Utils;
 
 namespace myTNB_Android.Src.Base.Fragments
@@ -23,14 +24,47 @@ namespace myTNB_Android.Src.Base.Fragments
 
         public event EventHandler<int> mCancelHandler;
 
+        private List<string> displayMonthList = new List<string>();
+
         public MonthYearPickerDialog(BaseAppCompatActivity ctx, int minYear, int maxYear, int minMonth, int maxMonth, DateTime selectedDateTime)
         {
             this.mContext = ctx;
             this.min_year = minYear;
             this.max_year = maxYear;
-            this.min_month = minMonth;
-            this.max_month = maxMonth;
+            if (minMonth > 0 && minMonth <= 12)
+            {
+                this.min_month = minMonth;
+            }
+            else
+            {
+                this.min_month = 1;
+            }
+
+            if (maxMonth > 0 && maxMonth <= 12)
+            {
+                this.max_month = maxMonth;
+            }
+            else
+            {
+                this.max_month = 12;
+            }
+
+            if (this.max_month < this.min_month)
+            {
+                this.max_month = this.min_month;
+            }
+
             mSelectedDateTime = selectedDateTime;
+
+            List<BaseKeyValueModel> monthList = Utility.GetLocalizedMonthSelectorLabel("months");
+
+            displayMonthList.Clear();
+
+            for (int i = this.min_month - 1; i < this.max_month; i++)
+            {
+                displayMonthList.Add(monthList[i].Value);
+            }
+
         }
 
         public void SetListener(DatePickerDialog.IOnDateSetListener listener)
@@ -59,8 +93,11 @@ namespace myTNB_Android.Src.Base.Fragments
                 nowDateTime = mSelectedDateTime;
             }
 
-            monthPicker.MinValue = this.min_month;
-            monthPicker.MaxValue = this.max_month;
+            monthPicker.MinValue = this.min_month - 1;
+            monthPicker.MaxValue = this.max_month - 1;
+            monthPicker.DescendantFocusability = DescendantFocusability.BlockDescendants;
+
+            monthPicker.SetDisplayedValues(displayMonthList.ToArray());
             monthPicker.Value = nowDateTime.Month;
 
             yearPicker.MinValue = this.min_year;
@@ -70,7 +107,7 @@ namespace myTNB_Android.Src.Base.Fragments
             builder.SetView(dialog)
                 .SetPositiveButton(Utility.GetLocalizedLabel("Common", "ok"), (senderAlert, args) =>
                 {
-                    mListener.OnDateSet(null, yearPicker.Value, monthPicker.Value, 0);
+                    mListener.OnDateSet(null, yearPicker.Value, monthPicker.Value + 1, 0);
                 })
                 .SetNegativeButton(Utility.GetLocalizedLabel("Common", "cancel"), (senderAlert, args) =>
                 {
