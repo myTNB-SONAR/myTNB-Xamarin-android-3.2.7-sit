7b72209b4390199c902b14b1489e497d17edf2ba
Muhammad Syahmi Shafawi <syahmishafawi@SyahmiShafawis-Mac-mini.local>
2020-08-05 01:42:48 +0800

    change font size consent  tooltip 12 to 11

--- myTNB.iOS/Feedback/Enquiry/Update Personal Detail/UpdatePersonalDetailViewController.cs
+++ myTNB.iOS/Feedback/Enquiry/Update Personal Detail/UpdatePersonalDetailViewController.cs
@@ -1214,7 +1214,7 @@ namespace myTNB
                 , GetScaledHeight(4), _ownerConsentToolTipsView.Frame.Width - GetScaledWidth(44), GetScaledHeight(16)))
             {
                 TextAlignment = UITextAlignment.Left,
-                Font = TNBFont.MuseoSans_12_500,
+                Font = TNBFont.MuseoSans_11_500,
                 TextColor = MyTNBColor.WaterBlue,
                 Text = GetI18NValue(EnquiryConstants.ownerConsentTitle) 
 
