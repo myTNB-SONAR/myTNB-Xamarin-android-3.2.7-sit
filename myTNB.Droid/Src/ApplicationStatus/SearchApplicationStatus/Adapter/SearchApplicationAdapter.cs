cc95871ff374488c9b4e9ca62adf8fcabe4d11c2
ron reyes <>
2020-12-03 00:26:14 +0800

    Update UI for Green Dot Indicator for updated Applications.

--- myTNB.Droid/Src/ApplicationStatus/SearchApplicationStatus/Adapter/SearchApplicationAdapter.cs
+++ myTNB.Droid/Src/ApplicationStatus/SearchApplicationStatus/Adapter/SearchApplicationAdapter.cs
@@ -1,14 +1,9 @@
 ﻿using Android.Content;
-using Android.Graphics;
-
-
 using Android.Views;
 using Android.Widget;
 using AndroidX.Core.Content;
 using AndroidX.RecyclerView.Widget;
-using myTNB.Mobile;
 using myTNB.Mobile.API.Models.ApplicationStatus.GetApplicationsByCA;
-using myTNB_Android.Src.ApplicationStatus.ApplicationStatusListing.Models;
 using myTNB_Android.Src.Base.Activity;
 using myTNB_Android.Src.Utils;
 using System;
@@ -22,7 +17,6 @@ namespace myTNB_Android.Src.ApplicationStatus.SearchApplicationStatus.Adapter
         //private List<ApplicationStatusColorCodeModel> mStatusCodeColorList = new List<ApplicationStatusColorCodeModel>();
         private List<GetApplicationsByCAModel> mApplicationStatusList = new List<GetApplicationsByCAModel>();
         public event EventHandler<int> ItemClick;
-
 
         public void Clear()
         {
@@ -69,10 +63,11 @@ namespace myTNB_Android.Src.ApplicationStatus.SearchApplicationStatus.Adapter
         void OnClick(int position)
         {
             if (ItemClick != null)
+            {
                 ItemClick(this, position);
+            }
         }
     }
-
 
     public class ApplicationStatusViewHolder : RecyclerView.ViewHolder
     {
@@ -80,7 +75,6 @@ namespace myTNB_Android.Src.ApplicationStatus.SearchApplicationStatus.Adapter
         public TextView ApplicationStatusItemSubTitle { get; private set; }
         public LinearLayout AppicationStatusListMainLayout { get; private set; }
         public TextView ApplicationStatusItemStatus { get; private set; }
-        public TextView ApplicationStatusItemDate { get; private set; }
         public ImageView ApplicationStatusItemNewIndicator { get; private set; }
         public ImageView ApplicationStatusItemRightArrow { get; private set; }
         private Context context;
@@ -94,44 +88,25 @@ namespace myTNB_Android.Src.ApplicationStatus.SearchApplicationStatus.Adapter
             ApplicationStatusItemTitle = itemView.FindViewById<TextView>(Resource.Id.applicationStatusItemTitle);
             ApplicationStatusItemSubTitle = itemView.FindViewById<TextView>(Resource.Id.applicationStatusItemSubTitle);
             ApplicationStatusItemStatus = itemView.FindViewById<TextView>(Resource.Id.applicationStatusItemStatus);
-            ApplicationStatusItemDate = itemView.FindViewById<TextView>(Resource.Id.applicationStatusItemDate);
             ApplicationStatusItemNewIndicator = itemView.FindViewById<ImageView>(Resource.Id.applicationStatusItemNewIndicator);
             ApplicationStatusItemRightArrow = itemView.FindViewById<ImageView>(Resource.Id.applicationStatusItemRightArrow);
             ApplicationStatusItemTitle.SetTextColor(new Android.Graphics.Color(ContextCompat.GetColor(this.context, Resource.Color.tunaGrey)));
             ApplicationStatusItemSubTitle.SetTextColor(new Android.Graphics.Color(ContextCompat.GetColor(this.context, Resource.Color.receipt_note_text)));
             ApplicationStatusItemStatus.SetTextColor(new Android.Graphics.Color(ContextCompat.GetColor(this.context, Resource.Color.silverChalice)));
-            ApplicationStatusItemDate.SetTextColor(new Android.Graphics.Color(ContextCompat.GetColor(this.context, Resource.Color.receipt_note_text)));
             TextViewUtils.SetMuseoSans300Typeface(ApplicationStatusItemSubTitle);
-            TextViewUtils.SetMuseoSans500Typeface(ApplicationStatusItemTitle, ApplicationStatusItemStatus, ApplicationStatusItemDate);
+            TextViewUtils.SetMuseoSans500Typeface(ApplicationStatusItemTitle, ApplicationStatusItemStatus);
             AppicationStatusListMainLayout.Click += (sender, e) => listener(base.LayoutPosition);
         }
-
 
         public void PopulateData(GetApplicationsByCAModel item)
         {
             this.item = item;
             try
             {
-                //if (this.item.IsUpdated)
-                //{
-
-                //    ApplicationStatusItemNewIndicator.Visibility = ViewStates.Visible;
-                //    ApplicationStatusItemDate.SetTextColor(new Android.Graphics.Color(ContextCompat.GetColor(this.context, Resource.Color.tunaGrey)));
-                //}
-                //else
-                //{
-                //    ApplicationStatusItemNewIndicator.Visibility = ViewStates.Gone;
-                //    ApplicationStatusItemDate.SetTextColor(new Android.Graphics.Color(ContextCompat.GetColor(this.context, Resource.Color.receipt_note_text)));
-                //}
-
                 ApplicationStatusItemTitle.Text = this.item.ApplicationTypeDisplay;
                 ApplicationStatusItemStatus.Text = this.item.StatusDisplay;
                 ApplicationStatusItemSubTitle.Text = this.item.ReferenceIDDisplay;
-                ApplicationStatusItemDate.Visibility = ViewStates.Gone;
-                //ApplicationStatusItemDate.Text = this.item.CreatedDate.ToString();
-                ApplicationStatusItemStatus.SetTextColor(Android.Graphics.Color.Rgb(this.item.StatusColor[0],this.item.StatusColor[1],this.item.StatusColor[2]));
-               
-
+                ApplicationStatusItemStatus.SetTextColor(Android.Graphics.Color.Rgb(this.item.StatusColor[0], this.item.StatusColor[1], this.item.StatusColor[2]));
             }
             catch (Exception e)
             {
