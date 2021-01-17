f5ac5ce4089df0bb317ca73b458d4e5726adeac2
ron reyes <>
2021-01-17 22:12:47 +0800

    Notification Updates

--- myTNB.Mobile/API/Models/Notification/Application Status/ApplicationStatusNotificationModel.cs
+++ myTNB.Mobile/API/Models/Notification/Application Status/ApplicationStatusNotificationModel.cs
@@ -5,10 +5,12 @@
         public string SaveApplicationID { set; get; } = string.Empty;
         public string ApplicationID { set; get; } = string.Empty;
         public string ApplicationType { set; get; } = string.Empty;
+        public string System { set; get; } = string.Empty;
 
         public static string TYPE_APPLICATIONSTATUS = "APPLICATIONSTATUS";
-        public static string TYPE_SAVEAPPLICATIONID = "SaveApplicationId";
-        public static string TYPE_APPLICATIONID = "ApplicationID";
-        public static string TYPE_APPLICATIONTYPE = "ApplicationType";
+        public static string Param_SAVEAPPLICATIONID = "SaveApplicationId";
+        public static string Param_APPLICATIONID = "ApplicationID";
+        public static string Param_APPLICATIONTYPE = "ApplicationType";
+        public static string Param_System = "System";
     }
 }
