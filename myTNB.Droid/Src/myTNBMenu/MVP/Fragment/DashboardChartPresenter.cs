209c69a326a159cbf138d4b9ca09ac5152055c07
syazwan <syazwan@2enapps.my>
2021-03-19 12:50:02 +0800

    fix bug chart

--- myTNB.Droid/Src/myTNBMenu/MVP/Fragment/DashboardChartPresenter.cs
+++ myTNB.Droid/Src/myTNBMenu/MVP/Fragment/DashboardChartPresenter.cs
@@ -696,8 +696,9 @@ namespace myTNB_Android.Src.myTNBMenu.MVP.Fragment
                     {
                         this.mView.OnSetBackendTariffDisabled(true);
                     }
-                    //this.mView.SetISMDMSDown(false);
-                    this.mView.SetISMDMSDown(true);     //debug data
+
+                    this.mView.SetISMDMSDown(false);
+                    //this.mView.SetISMDMSDown(true);     //debug data
                     this.mView.SetSMUsageData(usageHistoryResponse.Data.SMUsageHistoryData);
                     OnByRM();
                 }
