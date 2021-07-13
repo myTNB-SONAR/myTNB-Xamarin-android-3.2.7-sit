64c9ae3faf31fad7e89291a02cd5de04e9ee3636
ron reyes <>
2021-07-13 19:30:32 +0800

    FT: Updated Mobile Project

--- myTNB.Mobile/Business/Managers/AppInfoManager.cs
+++ myTNB.Mobile/Business/Managers/AppInfoManager.cs
@@ -33,19 +33,10 @@ namespace myTNB
         /// <param name="roleID">0 for All 16 for Consumers</param>
         /// <param name="userID">SSPUID</param>
         /// <param name="userName">Email or EID</param>
+        /// <param name="deviceToken">UUDID</param>
+        /// <param name="appVersion">ie: 2.3.3</param>
+        /// <param name="fontSize">App Font size N or L</param>
         /// <param name="language">Language Selected in the App</param>
-        public void SetUserInfos(string roleID
-            , string userID
-            , string userName
-            , Language language = Language.EN)
-        {
-            this.Language = language;
-            RoleId = roleID ?? string.Empty;
-            UserId = userID ?? string.Empty;
-            UserName = userName ?? string.Empty;
-            Lang = this.Language.ToString();
-        }
-
         public void SetUserInfo(string roleID
             , string userID
             , string userName
