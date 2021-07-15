35a74b80ade129fdc2c33ed10433e835af38d844
Darshan <dkprajapati18@gmail.com>
2021-07-15 12:55:01 +0530

    Merge Language Translation, Resolve Android Issue

---
+++ myTNB.Droid/Src/OverVoltageClaim/Activity/OvervoltageClaim.cs
@@ -0,0 +1,281 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using CheeseBind;
+using myTNB_Android.Src.Base.Activity;
+using myTNB_Android.Src.OverVoltageClaimSuccessPage.Activity;
+using myTNB_Android.Src.Utils;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+
+
+namespace myTNB_Android.Src.OverVoltageClaim.Activity
+{
+    [Activity(Label = "@string/submitICFeedback_OverVoltageClaimTitle"
+          , ScreenOrientation = ScreenOrientation.Portrait
+                  , WindowSoftInputMode = SoftInput.AdjustPan
+          , Theme = "@style/Theme.FaultyStreetLamps")]
+   
+    public class OvervoltageClaim : BaseToolbarAppCompatActivity 
+    {
+        public bool IsINZeroStepTab = true;
+        [BindView(Resource.Id.webView)]
+        WebView webView;
+
+        [BindView(Resource.Id.txtstep1of2)]
+        TextView txtstep1of2;
+
+        MyTNBAppToolTipBuilder leaveDialog;
+        public override int ResourceId()
+        {
+            return Resource.Layout.OvervoltageClaim;
+        }
+
+        public override bool ShowCustomToolbarTitle()
+        {
+            return true;
+        }
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                SetToolBarTitle(Utility.GetLocalizedLabel("SubmitEnquiry", "overVoltageClaimTitle"));
+                SetUI();
+                Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+                
+                //Enabled Javascript in Websettings  
+
+            }
+            catch (Exception ex)
+            {
+
+            } 
+        }
+
+        public override void OnBackPressed()
+        {
+            //bool SetOnBack = false;
+            if(!IsINZeroStepTab)
+            {
+                webView.EvaluateJavascript("javascript:(function() { setTimeout(function() { $('#onBack').trigger('click'); },500); })();", null);
+            }
+            else
+            {
+               leaveDialog = MyTNBAppToolTipBuilder.Create(this, MyTNBAppToolTipBuilder.ToolTipType.NORMAL_WITH_HEADER_TWO_BUTTON)
+              .SetTitle(Utility.GetLocalizedLabel("SubmitEnquiry", "leaveDialouge"))
+              .SetMessage(Utility.GetLocalizedLabel("SubmitEnquiry", "leaveDialougeMessage"))
+              .SetCTALabel(Utility.GetLocalizedCommonLabel("cancel"))
+              .SetSecondaryCTALabel(Utility.GetLocalizedCommonLabel("yes"))
+              .SetSecondaryCTAaction(() => { base.OnBackPressed(); })
+              .Build();
+              leaveDialog.Show();
+              
+            }
+           // ;
+        }
+
+        public async void SetUI()
+        {
+            try
+            {
+                webView = FindViewById<WebView>(Resource.Id.webView);
+                txtstep1of2 = FindViewById<TextView>(Resource.Id.txtstep1of2);
+                WebSettings websettings = webView.Settings;
+                //SetWebViewClient with an instance of WebViewClientClass  
+                websettings.JavaScriptEnabled = true;
+                websettings.LoadWithOverviewMode = true;
+                websettings.UseWideViewPort = true;
+                websettings.AllowFileAccess = true;
+                websettings.AllowContentAccess = true;
+                websettings.AllowFileAccessFromFileURLs = true;
+                websettings.AllowUniversalAccessFromFileURLs = true;
+                websettings.DomStorageEnabled = true;
+                websettings.SetAppCacheEnabled(true);
+                websettings.JavaScriptEnabled = true; //for set the javascript
+                websettings.JavaScriptCanOpenWindowsAutomatically = true;
+                // webView.SetWebViewClient(new WebViewClient());
+                ShowProgressDialog();
+                webView.SetWebChromeClient(new WebViewClient(this, webView) { });              
+                webView.LoadUrl("https://serene-rosalind-a35967.netlify.app/"); //Live http://192.168.0.178:3000/
+                await Task.Delay(0);
+                //HideProgressDialog();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public void ShowProgressDialog()
+        {
+            try
+            {
+                LoadingOverlayUtils.OnRunLoadingAnimation(this);
+            }
+            catch (Exception e)
+            {
+                Utility.LoggingNonFatalError(e);
+            }
+        }
+        public void HideProgressDialog()
+        {
+            try
+            {
+                LoadingOverlayUtils.OnStopLoadingAnimation(this);
+            }
+            catch (Exception e)
+            {
+                Utility.LoggingNonFatalError(e);
+            }
+        }
+        internal async Task ResponseFromWebviewAsync(string message)
+        {
+            try
+            { 
+                var data = JsonConvert.DeserializeObject<DTOWebView>(message); 
+                if (data.currentScreen == "step0-screen" && data.nextScreen == "step1-screen")
+                {
+                    IsINZeroStepTab = false;
+                    txtstep1of2.Visibility = ViewStates.Visible;
+                    //txtstep1of2.Text = "Step 1 of 2";
+                    txtstep1of2.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "stepTitle1of2");
+
+
+                    //Intent OverVoltagClaim = new Intent(this, typeof(OverVoltageClaimSuccessPageActivity));
+                    //OverVoltagClaim.PutExtra("SerialNumber", "ABC");
+                    //StartActivity(OverVoltagClaim);
+                }
+                else if (data.currentScreen == "step1-screen" && data.nextScreen == "step0-screen")
+                {
+                    IsINZeroStepTab = true;
+                    txtstep1of2.Visibility = ViewStates.Gone;
+                    //txtstep1of2.Text = "Step 1 of 2";
+                    txtstep1of2.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "stepTitle1of2");
+                }
+                else if (data.currentScreen == "step1-screen" && data.nextScreen == "step2-screen")
+                {
+                    IsINZeroStepTab = false;
+                    txtstep1of2.Visibility = ViewStates.Visible;
+                    //txtstep1of2.Text = "Step 2 of 2";
+                    txtstep1of2.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "stepTitle2of2");
+
+                    try
+                    {
+                        var location = await Geolocation.GetLastKnownLocationAsync();
+
+                        if (location != null)
+                        {
+                            Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                            webView.EvaluateJavascript("javascript:(function() { setTimeout(function() { setLocation(" + location.Latitude + "," + location.Longitude + ") },100); })();", null);
+                        }
+                    }
+                    catch (FeatureNotSupportedException fnsEx)
+                    {
+                        // Handle not supported on device exception
+                    }
+                    catch (FeatureNotEnabledException fneEx)
+                    {
+                        // Handle not enabled on device exception
+                    }
+                    catch (PermissionException pEx)
+                    {
+                        // Handle permission exception
+                    }
+                    catch (Exception ex)
+                    {
+                        // Unable to get location
+                    }
+                }
+                else if (data.currentScreen == "step2-screen" && data.nextScreen == "step1-screen")
+                {
+                    IsINZeroStepTab = false;
+                    txtstep1of2.Visibility = ViewStates.Visible;
+                    //txtstep1of2.Text = "Step 1 of 2";
+                    txtstep1of2.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "stepTitle1of2");
+                }
+                else if (data.currentScreen == "step2-screen" && data.nextScreen == "submit-screen")
+                    {
+                    IsINZeroStepTab = false;
+                    txtstep1of2.Visibility = ViewStates.Visible;
+                    //txtstep1of2.Text = "Step 2 of 2";
+                    txtstep1of2.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "stepTitle2of2");
+                    var serviceNumber = data.serviceNumber;
+                    Intent OverVoltagClaim = new Intent(this, typeof(OverVoltageClaimSuccessPageActivity));
+                    OverVoltagClaim.PutExtra("SerialNumber", serviceNumber.Trim());
+                    StartActivity(OverVoltagClaim);
+                }
+                else
+                {
+                    base.OnBackPressed();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+         
+    }
+
+    internal class WebViewClient :  WebChromeClient
+    {
+        OvervoltageClaim overVoltageClaim;
+        private WebView webview;
+        //Give the host application a chance to take over the control when a new URL is about to be loaded in the current WebView.  
+         
+        public  WebViewClient(OvervoltageClaim overVoltageClaim, WebView webview)
+        {
+            this.overVoltageClaim = overVoltageClaim;
+            this.webview = webview;
+           
+        }
+        public override bool OnJsAlert(WebView view, string url, string message, JsResult result)
+        {
+            //return base.OnJsAlert(view, url, message, result);
+            result.Cancel();
+            overVoltageClaim.ResponseFromWebviewAsync(message);
+
+            return true;
+
+        }
+        public override void OnProgressChanged(WebView view, int newProgress)
+        {
+            base.OnProgressChanged(view, newProgress);
+
+            if (newProgress == 100)
+            {
+                this.overVoltageClaim.HideProgressDialog();
+            
+            }
+           
+        }
+
+
+    }
+
+    public class DTOWebView
+    {
+        public string currentScreen { get; set; }
+        public string nextScreen { get; set; }
+        public string serviceNumber { get; set; }
+    }
+}
---
+++ myTNB.Droid/Src/OverVoltageClaimSuccessPage/Activity/OverVoltageClaimSuccessPageActivity.cs
@@ -0,0 +1,108 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CheeseBind;
+using myTNB_Android.Src.AppLaunch.Activity;
+using myTNB_Android.Src.Base.Activity;
+using myTNB_Android.Src.myTNBMenu.Activity;
+using myTNB_Android.Src.Utils;
+
+namespace myTNB_Android.Src.OverVoltageClaimSuccessPage.Activity
+{
+    [Activity(Label = "OverVoltageClaimSuccessPage"
+          , ScreenOrientation = ScreenOrientation.Portrait
+                  , WindowSoftInputMode = SoftInput.AdjustPan
+          , Theme = "@style/Theme.FaultyStreetLamps")]
+    public class OverVoltageClaimSuccessPageActivity : BaseAppCompatActivity
+    {
+
+        [BindView(Resource.Id.txtTitleInfo)]
+        TextView txtTitleInfo;
+
+        [BindView(Resource.Id.txtContentInfo)]
+        TextView txtContentInfo;
+
+        [BindView(Resource.Id.txtFeedbackIdContent)]
+        TextView txtFeedbackIdContent;
+
+        [BindView(Resource.Id.txtFeedbackIdTitle)]
+        TextView txtFeedbackIdTitle;
+
+        [BindView(Resource.Id.buttonBackToHome)]
+        Button buttonBackToHome;
+
+        [BindView(Resource.Id.btnViewSubmitted)]
+        Button btnViewSubmitted;
+
+
+
+        public override int ResourceId()
+        {
+            return Resource.Layout.OverVoltageClaimSuccessPage;
+        }
+
+
+        public override void OnBackPressed()
+        {
+            try
+            {
+                Intent intent = new Intent(this, typeof(DashboardHomeActivity));
+                // generalEnquiry.PutExtra(Constants.ACCOUNT_NUMBER, txtAccountNo.Text.ToString().Trim());           
+                intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.ClearTask | ActivityFlags.NewTask);
+                StartActivity(intent);
+            }
+            catch (Exception ex)
+            {
+
+            }        
+
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            try
+            {
+                // string SerialNumber =  Intent.GetStringExtra("SerialNumber");
+                var SerialNumber = Intent.GetStringExtra("SerialNumber");
+                txtFeedbackIdContent.Text = SerialNumber;
+                txtTitleInfo.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "submitICFeedback_OverVoltageClainSuccessPageThankyouRequest");
+                txtContentInfo.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "submitICFeedback_OverVoltageClainSuccessPageThankyouRequestContent");
+                txtFeedbackIdTitle.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "serviceNoTitle");
+                buttonBackToHome.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "backHomeButton");
+                btnViewSubmitted.Text = Utility.GetLocalizedLabel("SubmitEnquiry", "viewSubmittedEnquiry");
+                SetUI();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void SetUI()
+        {
+            //buttonBackToHome.Click += ButtonBackToHome_Click;
+        }
+
+      
+
+        [OnClick(Resource.Id.buttonBackToHome)]
+        void OnToHome(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(this, typeof(DashboardHomeActivity));            
+            intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.ClearTask | ActivityFlags.NewTask);
+            StartActivity(intent);
+        }
+    }
+}
