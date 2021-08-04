88b5444efeb0498f6505af56641de8c85dbf022c
ron reyes <>
2021-08-05 02:43:19 +0800

    Dynatrace Utility

---
+++ myTNB.Droid/Src/Utils/DynatraceHelper.cs
@@ -0,0 +1,21 @@
+﻿using DynatraceAndroid;
+using myTNB_Android.Src.Utils;
+
+namespace myTNB_Android
+{
+    internal static class DynatraceHelper
+    {
+        internal static void OnTrack(string actionName)
+        {
+            try
+            {
+                IDTXAction dynaTrace = DynatraceAndroid.Dynatrace.EnterAction(actionName);
+                dynaTrace.LeaveAction();
+            }
+            catch (System.Exception e)
+            {
+                Utility.LoggingNonFatalError(e);
+            }
+        }
+    }
+}
