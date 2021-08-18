28fcdc531be58aa96ba193f77b2066ddf8274209
Raja <rajasekhar.gajula@accenture.com>
2021-08-18 11:06:35 +0800

    Fix: Bug 5190: Tutorial Overlay Box Not Align (Android EN - Large Font)
    Fix: Bug 5190: Tutorial Overlay Box Not Align (Android EN - Large Font)

--- myTNB.Droid/Src/NewAppTutorial/Adapter/NewAppTutorialPagerAdapter.cs
+++ myTNB.Droid/Src/NewAppTutorial/Adapter/NewAppTutorialPagerAdapter.cs
@@ -470,8 +470,8 @@ namespace myTNB_Android.Src.NewAppTutorial.Adapter
                             if (((HomeMenuFragment)this.mFragment).CheckIsScrollable())
                             {
                                 int belowHeight = ((myTNB_Android.Src.myTNBMenu.Activity.DashboardHomeActivity)this.mContext).BottomNavigationViewHeight() + (TextViewUtils.IsLargeFonts ? 0 : ((HomeMenuFragment)this.mFragment).GetnewFAQTitleHeight());
-                                middleHeight += (int)DPUtils.ConvertDPToPx(5f);
-                                topHeight = (this.mContext.Resources.DisplayMetrics.HeightPixels - belowHeight) - middleHeight - ((HomeMenuFragment)this.mFragment).GetnewFAQTitleHeight() - (TextViewUtils.IsLargeFonts ? (int)DPUtils.ConvertDPToPx(15f) : (int)DPUtils.ConvertDPToPx(15f));
+                                middleHeight += (int)DPUtils.ConvertDPToPx(15f);
+                                topHeight = (this.mContext.Resources.DisplayMetrics.HeightPixels - belowHeight) - middleHeight - ((HomeMenuFragment)this.mFragment).GetnewFAQTitleHeight() - (TextViewUtils.IsLargeFonts ? (int)DPUtils.ConvertDPToPx(11f) : (int)DPUtils.ConvertDPToPx(-7f));
                             }
 
                             LinearLayout.LayoutParams topLayoutParam = topLayout.LayoutParameters as LinearLayout.LayoutParams;
