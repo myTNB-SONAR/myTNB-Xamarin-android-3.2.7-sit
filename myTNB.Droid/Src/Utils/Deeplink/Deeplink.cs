43d84b91051b99cff7d8a0734eb55b302332caa6
rob <roberto.f.cestina.jr@accenture.com>
2021-09-15 17:25:12 +0800

    QR Code - Added new utility for Deeplink and new class for handling deeplink related constants and enums

---
+++ myTNB.Droid/Src/Utils/Deeplink/Deeplink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace myTNB_Android.Src.Utils.Deeplink
+{
+    public class Deeplink
+    {
+        public class Constants
+        {
+            internal static readonly string WhatsNewIDKey = "wnid";
+            internal static readonly string RewardsIDKey = "rid";
+            internal static readonly string GetBillIDKey = "CA";
+
+            internal static readonly string Pattern = "\\b{0}.*\\b";
+            internal static readonly string ReplaceKey = "{0}=";
+            internal static readonly string Slash = "/";
+            internal static readonly string AmperSand = "&";
+        }
+
+        public enum ScreenEnum
+        {
+            Rewards,
+            WhatsNew,
+            ApplicationListing,
+            QR,
+            GetBill,
+            None
+        }
+    }
+}
