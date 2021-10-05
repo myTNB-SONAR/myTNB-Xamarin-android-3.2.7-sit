a239071a38a85736a326e1c897bcfaa41e49d973
Mohamad Syazwan b. Zoo <mohamad.syazwanICS@tnb.com.my>
2021-10-05 15:54:48 +0800

    api applaunch aws pointing change

--- myTNB.Mobile/MobileConstants.cs
+++ myTNB.Mobile/MobileConstants.cs
@@ -47,10 +47,9 @@ namespace myTNB.Mobile
         private const string Passphrase_PROD = "PW-myTNB-PROD";
 
         //AWS GetAccount
-        public static string AWSApiDomainSIT = "https://core.stg-mytnb.com/api"; 
+        public static string AWSApiDomainSIT = "https://stagingapi.mytnb.com.my/core/api";
         public static string AWSApiDomainDEV = "https://mytnb-core-staging-362772745.ap-southeast-1.elb.amazonaws.com/api";
-        public static string AWSApiDomainPROD = "https://core.prod-mytnb.com/api";  
- 
+        public static string AWSApiDomainPROD = "https://api.mytnb.com.my/core/api";
 
 #if DEBUG
         public static string ApiDomain = SIT;
