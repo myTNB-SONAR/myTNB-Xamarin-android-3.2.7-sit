f08197a124e3291fc0fe52626638bb9961546d58
rob <roberto.f.cestina.jr@accenture.com>
2021-10-28 22:54:44 +0800

    updated StatusCode for GSL

--- myTNB.Droid/Src/Base/Models/SubmittedFeedback.cs
+++ myTNB.Droid/Src/Base/Models/SubmittedFeedback.cs
@@ -67,6 +67,56 @@ namespace myTNB_Android.Src.Base.Models
             }
         }
 
+        [JsonIgnore]
+        public EnquiryGSLStatusCode GSLStatusCode
+        {
+            get
+            {
+                return StatusCode switch
+                {
+                    "GS01" => EnquiryGSLStatusCode.GS01,//Created***
+                    "GL01" => EnquiryGSLStatusCode.GL01,//Created***
+                    "GS08" => EnquiryGSLStatusCode.GS08,//Closed***
+                    "GL05" => EnquiryGSLStatusCode.GL05,//Closed***
+                    "GS06" => EnquiryGSLStatusCode.GS06,//Accepted***
+                    "GL03" => EnquiryGSLStatusCode.GL03,//Accepted
+                    "GS07" => EnquiryGSLStatusCode.GS07,//Rejected***
+                    "GL04" => EnquiryGSLStatusCode.GL04,//Rejected***
+                    "GS05" => EnquiryGSLStatusCode.GS05,//Rejected***
+                    "GS02" => EnquiryGSLStatusCode.GS02,//In Progress***
+                    "GL02" => EnquiryGSLStatusCode.GL02,//In Progress***
+                    "GS03" => EnquiryGSLStatusCode.GS03,//Pending Approval***
+                    "GS04" => EnquiryGSLStatusCode.GS04,//Approved***
+                    _ => EnquiryGSLStatusCode.NONE,
+                };
+            }
+        }
+
+        [JsonIgnore]
+        public int GSLStatusColor
+        {
+            get
+            {
+                return GSLStatusCode switch
+                {
+                    EnquiryGSLStatusCode.GS01 => Resource.Color.createdColorSubmit,
+                    EnquiryGSLStatusCode.GL01 => Resource.Color.createdColorSubmit,
+                    EnquiryGSLStatusCode.GS02 => Resource.Color.inProgressColor,
+                    EnquiryGSLStatusCode.GL02 => Resource.Color.inProgressColor,
+                    EnquiryGSLStatusCode.GS03 => Resource.Color.inProgressColor,
+                    EnquiryGSLStatusCode.GS06 => Resource.Color.inProgressColor,
+                    EnquiryGSLStatusCode.GL03 => Resource.Color.inProgressColor,
+                    EnquiryGSLStatusCode.GS04 => Resource.Color.completedColor,
+                    EnquiryGSLStatusCode.GS08 => Resource.Color.completedColor,
+                    EnquiryGSLStatusCode.GL05 => Resource.Color.completedColor,
+                    EnquiryGSLStatusCode.GS07 => Resource.Color.cancelledColor,
+                    EnquiryGSLStatusCode.GL04 => Resource.Color.cancelledColor,
+                    EnquiryGSLStatusCode.GS05 => Resource.Color.cancelledColor,
+                    _ => Resource.Color.createdColorSubmit,
+                };
+            }
+        }
+
         public static SubmittedFeedback Copy(SubmittedFeedbackEntity entity)
         {
             return new SubmittedFeedback()
