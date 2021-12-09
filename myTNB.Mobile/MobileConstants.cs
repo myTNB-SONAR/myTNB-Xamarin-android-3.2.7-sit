376479a50a04cda744e9c90a7ec59bbcf9aa4953
ron reyes <>
2021-12-10 00:11:11 +0800

    Update environment to on-prem. Version Up

--- myTNB.Mobile/MobileConstants.cs
+++ myTNB.Mobile/MobileConstants.cs
@@ -38,9 +38,9 @@ namespace myTNB.Mobile
         private const string DEV3 = "http://10.215.128.191:97";
 
         //ASMX
-        //private const string SIT = "https://mobiletestingws.tnb.com.my";
+        private const string SIT = "https://mobiletestingws.tnb.com.my";
         //AWS ASMX Copy
-        private const string SIT = "https://stagingapi.mytnb.com.my/asmx-97";
+        //private const string SIT = "https://stagingapi.mytnb.com.my/asmx-97";
         //AWS OVIS
         //private const string SIT = "http://mytnbwsovisstg.ap.ngrok.io";
         private const string PROD = "https://mytnbapp.tnb.com.my";
